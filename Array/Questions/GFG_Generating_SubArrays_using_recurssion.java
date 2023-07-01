9107ed1860c24f549dcda21878eee8c69f9ba3a2
DataBase Error <72812113+pushkarkumarlal@users.noreply.github.com>
2023-07-01 12:40:38 +0530

    Create GFG_Generating_SubArrays_using_recurssion.java

---
+++ Array/Questions/GFG_Generating_SubArrays_using_recurssion.java
@@ -0,0 +1,11 @@
+/*
+# Generating subarrays using recursion
+Given an array, generate all the possible subarrays of the given array using recursion.
+Examples: 
+
+Input : [1, 2, 3]
+Output : [1], [1, 2], [2], [1, 2, 3], [2, 3], [3]
+
+Input : [1, 2]
+Output : [1], [1, 2], [2]
+*/
