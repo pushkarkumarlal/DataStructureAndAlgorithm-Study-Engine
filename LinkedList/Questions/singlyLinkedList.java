82dfc69fcd62a7b4cb383e049ee5fa971c0c2661
DataBase Error <72812113+pushkarkumarlal@users.noreply.github.com>
2023-07-03 21:59:34 +0530

    Create singlyLinkedList.java

---
+++ LinkedList/Questions/singlyLinkedList.java
@@ -0,0 +1,12 @@
+// Create a singly Linked List and Perform its common Operation
+
+
+class LinkedList{
+private int=new int();
+LinkedList node=New LinkedList();
+}
+class Main{
+Public static void main(String[] args){
+
+}
+}
