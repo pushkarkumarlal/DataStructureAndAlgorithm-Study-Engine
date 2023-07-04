2916ec711f0225031a79ab3f294a0f518fde311c
DataBase Error <72812113+pushkarkumarlal@users.noreply.github.com>
2023-07-04 19:37:15 +0530

    Update singlyLinkedList.java
    updated the code for the singly linked list

--- LinkedList/Questions/singlyLinkedList.java
+++ LinkedList/Questions/singlyLinkedList.java
@@ -1,12 +1,67 @@
 // Create a singly Linked List and Perform its common Operation
+// 1- AddData
+// 2- Display Data
 
 
-class LinkedList{
-private int=new int();
-LinkedList node=New LinkedList();
+class singlyLinkedList{
+    
+    class node{
+        int data;
+        node next;
+        
+        public node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    
+    public node head=null;
+    public node tail=null;
+    
+    public void addNode(int data){
+        node newNode=new node(data);
+        
+        if(head==null){
+            head=newNode;
+            tail=newNode;
+            
+        }else{
+            tail.next=newNode;
+            tail=newNode;
+        }
+        
+        
+    }
+    
+    public void display(){
+        
+        node current=head;
+        
+        if(head==null){
+        System.out.println("The linkedlist is empty");
+            return;
+        }
+        else{
+            while(current!=null){
+                System.out.println(current.data +" ");
+                current=current.next;
+            }
+        }
+    }
 }
+
 class Main{
-Public static void main(String[] args){
-
-}
+    public static void main(String[] args){
+        // java code for singly linked list
+        
+        singlyLinkedList LinkedList=new singlyLinkedList();
+        
+        LinkedList.addNode(6);
+        LinkedList.addNode(7);
+        LinkedList.addNode(9);
+        LinkedList.addNode(6);
+        
+        LinkedList.display();
+        
+    }
 }
